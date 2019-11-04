378894f7a10ea945a22956e7cf9dee267c8a06ae
Mason Mackaman <masondeanm@aol.com>
2019-11-04 17:54:06 -0500

    Fix bug with "extends"
    The previous way of doing things broke when using a template that
    extended another. The new way of doing things isn't as compact,
    unfortunately.

--- index.js
+++ index.js
@@ -1,30 +1,4 @@
 const pug = require(`pug`);
-
-function ii(literals, ...args) {
-	let str = '';
-	for (var i = 0; i < args.length; i++) {
-		let literal = literals[i].match(/(?<rest>.*?)(?<tabs>\t*)$/s).groups;
-		str += literal.rest;
-		str += String(args[i]).split('\n').map(line => literal.tabs + line).join('\n');
-	}
-
-	str += literals[i];
-	function leadingTabs(str) {
-		return str.match(/^\t*/)[0].length;
-	}
-
-	let
-		lines = str.split('\n'),
-		addedIndent = Number(lines[0][0]) || 0,
-		baseIndent = leadingTabs(lines[1]);
-
-	return lines.slice(1, -1)
-		.map(line => {
-			let tabNum = Math.max(leadingTabs(line) - baseIndent + addedIndent, 0);
-			return '\t'.repeat(tabNum) + line.replace(/^\t*/, '');
-		}).join('\n');
-}
-
 module.exports = function(pugs, dir, options = {}) {
 	const
 		{
@@ -36,30 +10,33 @@ module.exports = function(pugs, dir, options = {}) {
 			doctype: `html`,
 		}, options.options);
 
-	let bigPug = `case ${compileClientOptions.self ? `self.` : ``}__pug_template_name\n`;
-	for (let pugObj of pugs) {
-		bigPug += ii`1
-			when '${pugObj.name}'
-				${pugObj.pug}\n
+	let funcStr = `
+			${module === true ? `export default ` : ``}function ${name}(name, locals, string = false) {
+				const templates = {
 		`;
-	}
 
 	const pwd = process.cwd();
 	process.chdir(dir);
-		const pugFunction = pug.compileClient(bigPug, compileClientOptions);
-	process.chdir(pwd);
-	return ii`
-		${module === true ? `export default ` : ``}function ${name}(name, locals, string = false) {
-			${pugFunction}
-
-			const __html = template(Object.assign({__pug_template_name: name}, locals));
-			if (string) {
-				return __html;
-			} else {
-				const templateElem = document.createElement('template');
-				templateElem.innerHTML = __html;
-				return templateElem.content;
-			}
+		for (let pugObj of pugs) {
+			funcStr += `
+				'${pugObj.name}': function(...args) {
+					${pug.compileClient(pugObj.pug, compileClientOptions)}
+					return template(...args);
+				},
+			`;
 		}
-	`;
+	process.chdir(pwd);
+	return funcStr + `
+		};
+
+		const html = templates[name](locals);
+		if (string) {
+			return html;
+		} else {
+			const templateElem = document.createElement('template');
+			templateElem.innerHTML = html;
+			return templateElem.content;
+		}
+	}
+	`
 }
